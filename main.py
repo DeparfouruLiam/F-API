9d1f113670ba5db86594478ddb508cc11c0a31f4
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-10-14 16:31:50 +0200

    Added : Base of app, classes and dictionnÃaries

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 from typing import TypedDict
+from fastapi import FastAPI, Depends
 app = FastAPI()
 
+class Transaction(TypedDict):
+    ibanSender: str
+    ibanReceiver: str
+    amount: int
 
-@app.get("/")
-async def root():
-    return {"message": "Gooning world"}
+class Account(TypedDict):
+    amount: int
+    iban: str
+    transactions: list[Transaction]
+
+class Beneficiary(TypedDict):
+    username: str
+    iban: str
+
+class Users(TypedDict):
+    username: str
+    password: str
+    accounts: list[Account]
+    mainAccount: Account
+    Beneficiaries: list[Beneficiary]
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
-class Item(BaseModel):
-    name: str
-    price: float
+users = [{"username": "Liam", "password": "furryfemboy", "accounts": [],"mainAccount":None,"Beneficiary": []},{"username": "Ghazi", "password": "jadorelespieds", "accounts": [],"mainAccount":None,"Beneficiary": []},{"username": "Adam", "password":"futa", "accounts": [],"mainAccount": None,"Beneficiary": []}]
+accounts = [{"amount": 100, "iban": "Aled", "transactions": []},{"amount": 10, "iban": "Yiouiuh", "transactions": []},{"amount": 50, "iban": "Buranyah", "transactions": []}]
+beneficiaries = [{"username": "Ghazi","iban": "Buranyah"}]
+transactions = [{"ibanSender": "Aled", "ibanReceiver": "Yiouiuh", "Amount":20}]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
 
-class Config(TypedDict):
-    version :str
-    name: str
+
+def get_accounts():
+    return accounts
+
+def get_amount(account):
+    return account["amount"]
+
+
+def account_from_iban(iban):
+    return next((x for x in get_accounts() if x["iban"] == iban), None)
+
+@app.get("/transfer")
+def transfer_amount(sender,receiver,amount):
+    sender = account_from_iban(sender)
+    receiver = account_from_iban(receiver)
+    sender.update({"amount": get_amount(sender) - int(amount)})
+    receiver.update({"amount": get_amount(receiver) + int(amount)})
+
+@app.get("/read_amount")
+def read_amount(user):
+    user = account_from_iban(user)
+    return {"Amount": user["amount"]}
